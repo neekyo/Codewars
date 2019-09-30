9a4a3960cbf368feb0e5e0eb38859d0419e62c67
Nick Sherman <ogleffeckt2@yahoo.com>
2019-09-29 21:01:36 -0400

    added kata solution

---
+++ 6 kyu - Mexican Wave.js
@@ -0,0 +1,12 @@
+function wave(text) {
+	let finalArray = [];
+	for (let i = 0; i < text.length; i++) {
+		let arr = text.split('');
+		if (arr[i] === ' ') {
+			continue;
+		}
+		arr[i] = arr[i].toUpperCase();
+		finalArray.push(arr.join(''));
+	}
+	return finalArray;
+}
